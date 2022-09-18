ded2d299f4d5e7475cd8b69ae5c9e5e383d44c04
ijaz35 <ijaz.hossain1@gmail.com>
2022-09-18 15:55:48 +0600

    module 29

---
+++ case-convert.js
@@ -0,0 +1,7 @@
+const song = 'amar sonar bangla ami tomai Valobasi';
+
+// console.log(searching.toUpperCase());
+// console.log(searching.toLowerCase());
+// const searching = song.indexOf('valobasi');
+// console.log(searching);
+console.log(song.toLowerCase().includes('Valobasi'.toLowerCase()));
---
+++ string-search.js
@@ -0,0 +1,44 @@
+const arrayOfProducts = [
+    'asus s510u laptop',
+    'dell inspring laptop',
+    'laptop lenovo 1x4e laptop',
+    'yellow screen Laptop fig',
+    'HTC one phone',
+    'samsung Phone',
+    'apple Laptop',
+    'iphone7 128 gb'
+];
+const searching = 'Laptop';
+const laptops = [];
+
+/* //searching by indexOf() method which returns index number, if there is no index it returns -1;
+for (const product of arrayOfProducts) {
+    if (product.toLowerCase().indexOf(searching.toLowerCase()) != -1) {
+        laptops.push(product);
+    }
+}
+console.log(laptops); */
+
+/* //searching by includes() method which returns true or false;
+for (const product of arrayOfProducts) {
+    if (product.toLowerCase().includes(searching.toLowerCase())) {
+        laptops.push(product);
+    }
+}
+console.log(laptops); */
+
+/* //searching by startsWith() method, used for to find some word at start
+for (const product of arrayOfProducts) {
+    if (product.toLowerCase().startsWith(searching.toLowerCase())) {
+        laptops.push(product);
+    }
+}
+console.log(laptops); */
+
+//searching by endssWith() method, used for to find some word at ends
+for (const product of arrayOfProducts) {
+    if (product.toLowerCase().endsWith('fig'.toLowerCase())) {
+        laptops.push(product);
+    }
+}
+console.log(laptops);
